294cb1fc6cd44b79d778bb1bcf4e1aaa4abf022a
noohv <valters.huuns@gmail.com>
2022-10-03 23:53:29 +0300

    Server side setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 
 function App() {
+const [backendData, setBackendData] = useState([{}])
+
+useEffect(() => {
+  fetch("/api").then(
+    res => res.json()
+  ).then(
+    data => {
+      setBackendData(data)
+    }
+  )
+}, [])
+
   return (
     <div>
-      
+      {(typeof backendData.users === "undefined") ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user,i) => (
+           <p key={i}>{user}</p>
+        ))
+      )}
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/api", (req,res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"] })
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})

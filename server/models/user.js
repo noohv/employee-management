615ddfd682ba39a579dbc13c47b4a7186dad508a
noohv <valters.huuns@gmail.com>
2022-12-30 15:20:43 +0200

    Update wording for email

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" },
+  role: { type: String, required: true, default: "user" },
   id: { type: String }
 })
 
